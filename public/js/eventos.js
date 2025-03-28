913f068dfaf62869ab8b20ea41890ee7aa47f79f
Alexandre <airtontunni@gmail.com>
2025-03-27 23:09:19 -0300

    bora que bora

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,7 +1,7 @@
 const eventos = [
-    { data: '2025-04-15', nome: 'Missa Dominical' },
-    { data: '2025-04-20', nome: 'Encontro de Jovens' },
-    { data: '2025-05-01', nome: 'Palestra sobre fé' }
+    { data: '05/04/2025', hora: "8h00", nome: 'Missa Dominical' },
+    { data: '10/04/2025', hora: "19h30", nome: 'Encontro de Jovens' },
+    { data: '2025-05-01', hora: "20h00", nome: 'Palestra sobre fé' }
 ];
 
 eventos.sort((a, b) => new Date(a.data) - new Date(b.data));
@@ -10,6 +10,6 @@ const eventList = document.getElementById('eventList');
 eventos.forEach(evento => {
     const li = document.createElement('li');
     li.className = 'list-group-item';
-    li.innerHTML = `<strong>${evento.data}</strong>: ${evento.nome}`;
+    li.innerHTML = `<strong>${evento.data}, ${evento.hora}</strong>: ${evento.nome}`;
     eventList.appendChild(li);
 });
