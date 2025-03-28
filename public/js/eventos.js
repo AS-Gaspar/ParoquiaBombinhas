dad5a78818ae7b200cffdb6fc26708d6b9cd91ba
Alexandre <airtontunni@gmail.com>
2025-03-28 11:29:48 -0300

    finalização da etapa 1 do trabalho

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,15 +1,16 @@
 const eventos = [
-    { data: '05/04/2025', hora: "8h00", nome: 'Missa Dominical' },
-    { data: '10/04/2025', hora: "19h30", nome: 'Encontro de Jovens' },
-    { data: '2025-05-01', hora: "20h00", nome: 'Palestra sobre fé' }
-];
+    { local: 'Paróquia N. Sra. da Imaculada Conceição', data: '05/04/2025', hora: '8h00', nome: 'Missa Dominical', descricao: 'Todos os domingos' },
+    { local: 'Paróquia N. Sra. da Imaculada Conceição', data: '10/04/2025', hora: '19h30', nome: 'Missa Ferial', descricao: 'Realizada em dias de semana e sábados'},
+    { local: 'Capela', data: '01/04/2025', hora: '20h00', nome: 'Encontro de Jovens', descricao: 'Todas as terças-feiras'},
+    { local: 'Salão de festas', data: '16/04/2025', hora: '14h00', nome: 'Bingo da Igreja', descricao: 'Acompanhado de almoço com costela'}
+]
 
-eventos.sort((a, b) => new Date(a.data) - new Date(b.data));
+eventos.sort((a, b) => new Date(a.data) - new Date(b.data))
 
-const eventList = document.getElementById('eventList');
+const eventList = document.getElementById('eventList')
 eventos.forEach(evento => {
-    const li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.innerHTML = `<strong>${evento.data}, ${evento.hora}</strong>: ${evento.nome}`;
-    eventList.appendChild(li);
-});
+    const li = document.createElement('li')
+    li.className = 'list-group-item'
+    li.innerHTML = `<strong>Local: </strong>${evento.local}</br> <strong>Data e Hora:</strong> ${evento.data} - ${evento.hora}</br> <strong>Evento:</strong> ${evento.nome}</br> <strong>Descrição:</strong> ${evento.descricao}.`
+    eventList.appendChild(li)
+})
