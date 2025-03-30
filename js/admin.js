2609717d73970816aea117e4bea350978a6488ea
Alexandre <airtontunni@gmail.com>
2025-03-30 12:11:30 -0300

    encerrando pela manhã

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,5 @@
+imp
+
 let eventos = []
 
 function renderEventos() {
@@ -6,7 +8,7 @@ function renderEventos() {
   eventos.sort((a, b) => new Date(a.data) - new Date(b.data))
   eventos.forEach((evento, index) => {
     const li = document.createElement('li')
-    li.className = 'list-group-item d-flex justify-content-between align-items-center'
+    li.className = 'list-group-item d-flex justify-content-between align-items-center gap-list'
     li.innerHTML = `<span><strong>${evento.data}</strong>: ${evento.nome}</span>
                         <span>
                           <button class="btn btn-sm btn-warning" onclick="editarEvento(${index})">Editar</button>
@@ -31,7 +33,7 @@ function removerEvento(index) {
 }
 
 function editarEvento(index) {
-  const novoData = prompt("Digite a nova data (YYYY-MM-DD):", eventos[index].data)
+  const novoData = prompt("Digite a nova data (DD-MM-YYYY):", eventos[index].data)
   const novoNome = prompt("Digite o novo nome do evento:", eventos[index].nome)
   if (novoData && novoNome) {
     eventos[index] = { data: novoData, nome: novoNome }
